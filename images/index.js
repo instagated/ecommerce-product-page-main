08b39ef4dbbe319c7c6a5c8acae86f590ddad313
PhatJack <tienphatng.693@gmail.com>
2024-01-14 16:17:23 +0700

    feat: add header

---
+++ images/index.js
@@ -0,0 +1,11 @@
+import Product1 from './image-product-1.jpg'
+import Product2 from './image-product-2.jpg'
+import Product3 from './image-product-3.jpg'
+import Product4 from './image-product-4.jpg'
+import ProductThumbNail1 from './image-product-1-thumbnail.jpg'
+import ProductThumbNail2 from './image-product-2-thumbnail.jpg'
+import ProductThumbNail3 from './image-product-3-thumbnail.jpg'
+import ProductThumbNail4 from './image-product-4-thumbnail.jpg'
+export { Product1, Product2, Product3, Product4, ProductThumbNail1, ProductThumbNail2, ProductThumbNail3, ProductThumbNail4 }
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Header from './components/Header'
 function App() {
 
 	return (
 		<>
+			<div className="w-full min-h-screen flex justify-center items-center">
+				<Header />
+			</div>
 		</>
 	)
 }
